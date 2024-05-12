09b826ea676c49b6293bc8d201876403044448c9
Miteos1986 <mdmitriy1986@gmail.com>
2024-05-12 22:33:16 +0200

    fix

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -5,9 +5,9 @@ const FriendList = ({friends}) => {
 return (
     <div className={styles.containerFriend}>
 <ul className={styles.listFriend} > 
-{friends.map((friend)=>{
-    return (<li className={styles.listItem} key={friend.id} >
-    <FriendListItem {...friend} />
+{friends.map(({avatar, id, name, isOnline})=>{
+    return (<li className={styles.listItem} key={id} >
+    <FriendListItem avatar={avatar} name={name} isOnline={isOnline}  />
 </li>)
 })}
 	
