8b62233039efcd77034ef7f8459acc617e0c409c
Miteos1986 <mdmitriy1986@gmail.com>
2024-05-16 16:14:57 +0200

    3 task

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import FriendList from "./components/FriendList/FriendList";
 import Profile from "./components/Profile/Profile";
 import userData from "./userData.json";
-import friends from "./friends.json"
+import friends from "./friends.json";
+import transactions from "./transactions.json"
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 
 
@@ -10,7 +12,7 @@ function App() {
   return (
     <>
  <div>
-  <Profile 
+  <Profile
   name={userData.username}
   tag={userData.tag}
   location={userData.location}
@@ -22,6 +24,7 @@ function App() {
  </div>
  <div>
  <FriendList friends={friends} />
+ <TransactionHistory items={transactions} />
  </div>
     </>
   );
